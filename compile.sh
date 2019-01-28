46f2928484ca4a4766711ff9508863c9e268f15f
Lauri Gustafsson <luutifa@jkry.org>
2019-01-28 21:52:03 +0200

    Add better optimization flags and assembler output

--- compile.sh
+++ compile.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-rustc -O -C prefer-dynamic test.rs -o test_rs
-g++ -O2 -std=c++11 test.cpp -o test_cpp
+rustc --emit asm -C opt-level=3 -C prefer-dynamic test.rs -o test_rs.s
+cc test_rs.s -l$(basename $(find /usr/lib -name '*libstd-*.so' -print -quit) | cut -c 4- | cut -c -20) -o test_rs
+g++ -S -Ofast -std=c++11 test.cpp -o test_cpp.s
+g++ test_cpp.s -o test_cpp
+chmod u+x test_cpp test_rs
 strip --strip-all test_rs test_cpp
